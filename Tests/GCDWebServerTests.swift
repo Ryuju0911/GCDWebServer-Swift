cb664fd95d19a2fecac1cdc8a6da4921e024c43a
Ryuju0911 <pinebook0911@gmail.com>
2023-08-22 15:20:22 +0900

    Add addHandlers and removeAllHandlers.

---
+++ Tests/GCDWebServerTests.swift
@@ -0,0 +1,17 @@
+@testable import GCDWebServer_Swift
+
+import XCTest
+
+final class Tests: XCTestCase {
+  
+  func testAddHandler() throws {
+    let server = GCDWebServer()
+    XCTAssertNotNil(server)
+    
+    server.addHandler()
+    XCTAssertEqual(server.handlersCount(), 1)
+    
+    server.removeAllHandlers()
+    XCTAssertEqual(server.handlersCount(), 0)
+  }
+}
